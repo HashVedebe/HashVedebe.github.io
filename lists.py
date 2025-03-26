e10d2a7e4d7432a429d5bf144d6dc747f57e797f
vdberkh <hilde.vandenberk@vrt.be>
2025-03-26 12:27:50 +0100

    Added lists.py

---
+++ lists.py
@@ -0,0 +1,24 @@
+#Define a list of names
+names = ["Alex", "Manu", "Hilde", "Wingo"]
+
+print(names)
+
+print(names[0])
+
+names.append("Poens")
+names.sort()
+print(names)
+
+# create an empty set: unique values, not the value 3 twice
+s = set()
+
+#Add elements to the set
+s.add(1)
+s.add(2)
+s.add(3)
+s.add(4)
+print(s)
+
+s.add(3)
+s.remove(2)
+print(s)
